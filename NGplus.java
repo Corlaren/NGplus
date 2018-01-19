5451e54e491d809fc64884360a48e6ca23ba3f10
Corlaren <gerocanzler@aol.com>
2018-01-19 14:55:50 +0100

    Version 0.2.1
    collision enhanced
    models enhanced

--- NGplus.java
+++ NGplus.java
@@ -1,12 +1,16 @@
 package ngPlus;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -18,23 +22,23 @@ import javax.swing.JFrame;
 import javax.swing.Timer;
 
 public class NGplus 
-implements ActionListener, KeyListener{
+implements ActionListener, KeyListener, MouseListener{
 	
 	public static NGplus ngPlus;
 	
 	public final int WIDTH = 800, HEIGHT = 800;
 	
-	public int xPos, yPos, yMotion, xMotion, counter, jumpCounter;
+	public int xPos, yPos, yMotion, xMotion, runFrame, idleFrame, fallTick, armed;
 	
-	public boolean xPlusStat, xMinusStat, yPlusStat, hitLeft, hitRight, hitTop, direction, grounded, armed;
+	public boolean moveLeft, moveRight, acsend, hitLeft, hitRight, hitTop, direction, grounded, jumped;
 	
 	public NGrenderer renderer;
 	
-	public Rectangle shadow;
+	public Rectangle shadow, proximityshadow;
 	
 	public Random rand;
 	
-	private BufferedImage heroIdleR, heroIdleL, heroIdleRAr, heroIdleLAr;
+	private BufferedImage heroIdleR, heroIdleL, heroRunningR, heroRunningL;
 	
 	public ArrayList<Rectangle> obstacles;
 	
@@ -50,47 +54,60 @@ implements ActionListener, KeyListener{
 		jframe.setTitle("Sol");
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.setSize(WIDTH, HEIGHT);
+		jframe.setUndecorated(true);
+		jframe.setAlwaysOnTop(true);
 		jframe.setResizable(false);
 		jframe.setVisible(true);
 		jframe.addKeyListener(this);
+		jframe.addMouseListener(this);
 		
-		xPlusStat = false;
-		xMinusStat = false;
-		yPlusStat = false;
+		makeFrameFullSize(jframe);
+		
+		moveLeft = false;
+		moveRight = false;
+		acsend = false;
+		jumped = false;
 		hitLeft = false;
 		hitRight = false;
 		hitTop = false;
-		armed = true;
 		direction = true;
 		grounded = true;
 		
+		runFrame = 0;
+		idleFrame = 0;
+		fallTick = 0;
 		xPos = 100;
-		yPos = 700;
-		yMotion = 1;
+		yPos = 600;
+		yMotion = 0;
 		xMotion = 0;
-		counter = 0;
-		jumpCounter = 0;
 		
 		obstacles = new ArrayList<Rectangle>();
 		
 		obstacles.add(new Rectangle(0, 722, 800, 78));
 		obstacles.add(new Rectangle(600, 700, 20, 25));
-		obstacles.add(new Rectangle(530, 650, 20, 20));
+		obstacles.add(new Rectangle(400, 700, 3, 25));
+		obstacles.add(new Rectangle(530, 630, 20, 20));
 		obstacles.add(new Rectangle(470, 620, 20, 20));
 		obstacles.add(new Rectangle(510, 580, 20, 20));
 		obstacles.add(new Rectangle(10, 400, 20, 400));
 		obstacles.add(new Rectangle(770, 400, 20, 400));
 		
 		try {
-			heroIdleR = ImageIO.read(new File("Hero_idle_R.png"));
-			heroIdleL = ImageIO.read(new File("Hero_idle_L.png"));
-			heroIdleRAr = ImageIO.read(new File("Hero_idle_R_Armed.png"));
-			heroIdleLAr = ImageIO.read(new File("Hero_idle_L_Armed.png"));
+			heroIdleR = ImageIO.read(new File("HeroIdleR.png"));
+			heroIdleL = ImageIO.read(new File("HeroIdleL.png"));
+			heroRunningR = ImageIO.read(new File("HeroRunningR.png"));
+			heroRunningL = ImageIO.read(new File("HeroRunningL.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
 		timer.start();
+	}
+	
+	private void makeFrameFullSize(JFrame aFrame) {
+		
+	    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	    aFrame.setSize(screenSize.width, screenSize.height);
 	}
 	
 	public void paintObstacle(Graphics g, Rectangle obstcl) {
@@ -101,76 +118,94 @@ implements ActionListener, KeyListener{
 	
 	public void repaint(Graphics g) {
 
-		g.setColor(Color.black);
-		g.fillRect(0, 0, WIDTH, HEIGHT);
+		g.setColor(Color.gray.darker());
+		g.fillRect(0, 0, 1980, 1080);
 		
 		for (Rectangle obstcl : obstacles) {
 			
 			paintObstacle(g, obstcl);
 		}
 		
-		
-		
 		if (direction) {
+
+			if (xMotion != 0 && grounded) {
 			
-			if (!armed) g.drawImage(heroIdleR, xPos, yPos, null);
-			else g.drawImage(heroIdleRAr, xPos, yPos, null);
+				g.drawImage(heroRunningR.getSubimage((runFrame/6)*30, 0, 30, 58), xPos, yPos, null);
+				runFrame++;
+				if (runFrame >= 47) runFrame = 0;
+			}
+			else if (!grounded) {
+				
+				g.drawImage(heroRunningR.getSubimage(30, 0, 30, 58), xPos, yPos, null);
+			}
+			else {
+
+				g.drawImage(heroIdleR.getSubimage((idleFrame/40)*30, 0, 30, 58), xPos, yPos, null);
+				idleFrame++;
+				if (idleFrame >= 79) idleFrame = 0;
+			}
 		}
 		else if (!direction) {
 
-			if (!armed) g.drawImage(heroIdleL, xPos, yPos, null);
-			else g.drawImage(heroIdleLAr, xPos-9, yPos, null);
+			if (xMotion != 0 && grounded) {
+			
+				g.drawImage(heroRunningL.getSubimage((runFrame/6)*30, 0, 30, 58), xPos, yPos, null);
+				runFrame--;
+				if (runFrame <= 0) runFrame = 47;
+			}
+			else if (!grounded) {
+				
+				g.drawImage(heroRunningL.getSubimage(180, 0, 30, 58), xPos, yPos, null);
+			}
+			else {
+				
+				g.drawImage(heroIdleL.getSubimage((idleFrame/40)*30, 0, 30, 58), xPos, yPos, null);
+				idleFrame++;
+				if (idleFrame >= 79) idleFrame = 0;
+			}
 		}
+		g.drawString(grounded + " ", 10, 10);
 		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		
-		if (counter == 12) {
+		if (moveLeft) {
 			
-			counter = 0;
-		}
-		if (jumpCounter == 48) {
-			
-			jumpCounter = 0;
+			xMotion--;
 		}
 		
-		if (xPlusStat) {
-			
-			if (grounded) xMotion--;
-			else if (counter % 6 == 0) xMotion--;
-			direction = false;
-		}
-		
-		if (xMinusStat) {
+		if (moveRight) {
 
-			if (grounded) xMotion++;
-			else if (counter % 6 == 0) xMotion++;
+			xMotion++;
+		}
+		
+		if (xMotion > 0) {
+
 			direction = true;
 		}
+		
+		if (xMotion < 0) {
 
-		if (yPlusStat && grounded) {
+			direction = false;
+		}
 
-			yPos--;
+		if (acsend && grounded && !jumped) {
+
 			yMotion = -5;
-			jumpCounter = 1;
+			fallTick = 0;
 			grounded = false;
+			jumped = true;
 		}
 		
-		if (yMotion < 10 && jumpCounter % 6 == 0) {
+		if (yMotion < 10) {
 			
-			yMotion++;
-		}
-		
-		if (yPos < 700) {
-			
-			yPos += yMotion;
-			
-			if (yPos >= 700) {
+			if (fallTick < 4) fallTick++;
+			else {
 				
-				yPos = 700;
-				grounded = true;
+				yMotion++;
+				fallTick = 0;
 			}
 		}
 		
@@ -184,54 +219,71 @@ implements ActionListener, KeyListener{
 			xMotion = -2;
 		}
 		
-		if (xMotion > 0 && !xMinusStat) {
+		if (xMotion > 0 && !moveRight) {
 			
-			if (grounded && counter %3 == 0) xMotion--;
-			else if (jumpCounter % 22 == 1) xMotion--;
+			xMotion--;
 		}
 		
-		if (xMotion < 0 && !xPlusStat) {
+		if (xMotion < 0 && !moveLeft) {
 			
-			if (grounded && counter %3 == 0) xMotion++;
-			else if (jumpCounter % 22 == 1) xMotion++;
+			xMotion++;
 		}
 
-		counter++;
-		jumpCounter++;
-		xPos += xMotion;
+		if (xMotion != 0 || yMotion != 0) collision(xPos, yPos, xMotion, yMotion);
 		
-		shadow = new Rectangle(xPos+1, yPos-1, 21, 24);
+		if (yMotion != 0) grounded = false;
+		
+		xPos += xMotion;
+		yPos += yMotion;
+		
+		renderer.repaint();
+	}
+	
+	public void collision (int xP, int yP, int xM, int yM) {
+		
+		int xCol = xPos;
+		int yCol = yPos;
 		
 		for (Rectangle obstcl : obstacles) {
 			
-			if (obstcl.intersects(shadow)) {
-				
-				if (shadow.x + (shadow.width / 2 + 2) < obstcl.x) {
+			proximityshadow = new Rectangle(xCol - 15, yCol - 15, 60, 88);
+			
+			if (obstcl.intersects(proximityshadow)) {
+			
+				for (int i = 1; i <= Math.max(Math.abs(xM), Math.abs(yM)); i++) {
 					
-					xPos = obstcl.x - shadow.width;
-					xMotion = 0;
-				}
-				
-				else if (shadow.x + (shadow.width / 2 - 2) > obstcl.x + obstcl.width) {
+					if (xMotion != 0) xCol = (int) (xP + (((xM * 1.001)/Math.max(Math.abs(xM), Math.abs(yM)))*i));
+					if (yMotion != 0) yCol = (int) (yP + (((yM * 1.001)/Math.max(Math.abs(xM), Math.abs(yM)))*i));
 					
-					xPos = obstcl.x + obstcl.width -1;
-					xMotion = 0;
-				}
-				else if (shadow.y > obstcl.y) {
+					shadow = new Rectangle(xCol + 8, yCol-1, 14, 59);
+			
+					if (obstcl.intersects(shadow)) {
 					
-					yPos = obstcl.y + obstcl.height;
-					yMotion = 0;
-				}
-				else {
-					
-					yPos = obstcl.y - 22;
-					yMotion = 0;
-					grounded = true;
+						if (xCol + 8 < obstcl.x + obstcl.width && xCol + 22 > obstcl.x && yCol + 50 < obstcl.y && yMotion >= 0) {
+						
+							yPos = yCol - 1;
+							yMotion = 0;
+							grounded = true;
+						}
+						else if (yCol + 2 > obstcl.y + obstcl.height && yMotion <= 0) {
+						
+							yPos = yCol;
+							yMotion = 0;
+						}
+						else if (xCol + 20 < obstcl.x) {
+						
+							xPos = xCol - 1;
+							xMotion = 0;
+						}
+						else {
+						
+							xPos = xCol + 2;
+							xMotion = 0;
+						}
+					}
 				}
 			}
 		}
-		
-		renderer.repaint();
 	}
 
 	public static void main(String[] args){
@@ -244,19 +296,27 @@ implements ActionListener, KeyListener{
 		
 		if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
 			
-			xPlusStat = true;
+			moveLeft = true;
 		}
 		else if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
 			
-			xMinusStat = true;
+			moveRight = true;
 		}
 		else if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
 			
-			yPlusStat = true;
+			acsend = true;
 		}
 		else if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
-			if (armed) armed = false;
-			else armed = true;
+			
+			
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+			
+			acsend = true;
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+			
+			System.exit(0);
 		}
 	}
 
@@ -265,21 +325,51 @@ implements ActionListener, KeyListener{
 		
 		if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
 			
-			xPlusStat = false;
+			moveLeft = false;
 		}
 		else if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
 			
-			xMinusStat = false;
+			moveRight = false;
 		}
 		else if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
 			
-			yPlusStat = false;
+			acsend = false;
+			jumped = false;
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+			
+			acsend = false;
+			jumped = false;
 		}
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
 		
 	}
 }
